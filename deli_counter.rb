9018cc46cd9d90f8fbc0d024d414725fa1bec8a8
Samantha Kane <s.kane133@gmail.com>
2019-06-12 22:54:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+kats_deli = []
+katz_deli = "The line is currently:"
+def line(num) 
+  if num.size == 0
+ puts "The line is currently empty."
+ 
+else
+   katz_deli.size > 0 
+  num.each_with_index do |line, index|
+    katz_deli << " #{index + 1}. #{line}"
+  end
+    puts katz_deli
+end
+end
+
+def take_a_number
+  
+end 
